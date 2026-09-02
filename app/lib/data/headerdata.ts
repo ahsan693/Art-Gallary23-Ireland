8ce0393ea89a9e319d77d1ad5fe18ce728b3fe24
ahsan693 <221387@students.au.edu.pk>
2026-09-03 00:20:19 +0500

    updated

--- app/lib/data/headerdata.ts
+++ app/lib/data/headerdata.ts
@@ -4,11 +4,13 @@ export const headerData = {
     { name: "Home", href: "/" },
     { name: "Services", href: "/Services" },
     { name: "Print Shop", href: "/printshop" },
-    { name: "Commercial", href: "/commercial" },
+   
   ],
   utilityNavigation: [
+     { name: "Commercial", href: "/commercial" },
     { name: "About Us", href: "/about" },
     { name: "Support", href: "/support" },
+    
   ],
   icons: {
     cart: "/gallery23/nav-shopping-bag.svg",
